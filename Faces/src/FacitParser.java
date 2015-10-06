2d2b354bdd2315ca81c1ea8be454e523692b0bf5
Johannes Ekman <ekman.johannes@gmail.com>
2015-10-06 13:13:27 +0200

    refactored. Började på FaceParser

---
+++ Faces/src/FacitParser.java
@@ -0,0 +1,8 @@
+
+import java.util.HashMap;
+
+public class FacitParser {
+	public static HashMap<String, Integer> parse(String fileName) throws Exception {
+		return new HashMap<String, Integer>();
+	}
+}
