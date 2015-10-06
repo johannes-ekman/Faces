5e59f1006212f53f04a4ec3499b7657bcb154fb3
Johannes Ekman <ekman.johannes@gmail.com>
2015-10-06 15:33:34 +0200

    Parsning av träning, facit och test bör vara klart

--- Faces/src/Face.java
+++ Faces/src/Face.java
@@ -2,4 +2,21 @@
 public class Face {
 	private int[][] dataPoints;
 	private String id;
+        
+        public Face (int[][] dataPoints, String id) {
+            this.dataPoints = dataPoints;
+            this.id = id;
+        }
+        public void printFace() {
+            System.out.println(id);
+            for(int i = 0; i < 20; i++) {
+                if(i !=0) System.out.println();
+                for(int j = 0; j < 20; j++) {
+                    System.out.print(dataPoints[i][j] + " ");
+                }
+            }
+            System.out.println();
+            System.out.println();
+            
+        }
 }
--- Faces/src/FacitParser.java
+++ Faces/src/FacitParser.java
@@ -1,8 +1,33 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FacitParser {
-	public static HashMap<String, Integer> parse(String fileName) throws Exception {
-		return new HashMap<String, Integer>();
-	}
+    public static HashMap<String, Integer> parse(String fileName) throws Exception {
+
+        FileReader input = new FileReader(fileName);
+        BufferedReader bufRead = new BufferedReader(input);
+        String line = null;
+        HashMap<String, Integer> facitMap = new HashMap<String, Integer>();
+
+
+        while ((line = bufRead.readLine()) != null) {  
+
+            if (line.startsWith("#")) {
+                // Do nothing
+            } else if (line.isEmpty() || line.trim().equals("") || line.trim().equals("\n")) {
+                // Do nothing
+            } else {
+                String[] words = line.split(" ");
+                String key = words[0];
+                int value = Integer.parseInt(words[1]);
+                System.out.println(key+" "+value);
+                facitMap.put(key, value);
+            }
+        }
+        bufRead.close();
+        return facitMap;
+    }
 }
